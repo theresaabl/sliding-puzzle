a4bf272e6fcd7754582fa0fe67385ffd0c660e7d
Theresa Abl <theresaabl@gmx.at>
2024-09-20 12:18:02 +0000

    Write function getTilesObjectArray to save positions and entries of puzzle tiles

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,19 @@
 /* jshint esversion: 11 */
 
+let gridSize = 3;  //later: get from user with getGridSize()
+
+
+  // HTML collection of tiles
+  let gridTiles = document.getElementsByClassName("tile-js");
+
+// // 1d array of tiles
+// let tilesArray = [];
+// for (let tile of gridTiles){
+//   tilesArray.push(tile.textContent);
+// }
 
 // Add event listeners for tiles
-
-// HTML collection of tiles
-let gridPositions = document.getElementsByClassName("position");
-console.log(gridPositions);
-// 1d array of tiles
-let gridSize = 3;
-let tilesArray1d = [];
-for (let tile of gridPositions){
-  tilesArray1d.push(tile.textContent);
-}
-console.log(tilesArray1d);
-// grid coordinates for gridSize
-let coordinates = [];
-for (let i = 0; i < gridSize; i ++){
-  for (let j = 0; j < gridSize; j++){
-    coordinates.push([i, j]);
-  }
-}
-console.log(coordinates);
-
-// array of tiles objects to save grid position and assigned number
-let tilesObjectArray = [];
-for (let i = 0; i < gridPositions.length; i++){
-  let tilesObject = {};
-  tilesObject.position = coordinates[i];
-  tilesObject.number = gridPositions[i].textContent;
-  tilesObjectArray.push(tilesObject);
-}
-console.log(tilesObjectArray);
-
-for (let tile of gridPositions) {
+for (let tile of gridTiles) {
   tile.addEventListener("click", function() {
     this.style.backgroundColor="red";
     console.log(this);
@@ -44,7 +25,34 @@ for (let tile of gridPositions) {
   });
 }
 
+/**
+ * This function takes the HTML collection of tiles in the puzzle grid
+ * and returns an array of tile objects which contain the coordinates 
+ * as well as displayed numbers of the tiles
+ * @param {*} gridTiles HTML collection of puzzle tiles
+ * @returns array of tile objects
+ */
+function getTilesObjectArray(gridTiles){
+  // grid coordinates for gridSize
+  let coordinates = [];
+  for (let i = 0; i < gridSize; i ++){
+    for (let j = 0; j < gridSize; j++){
+      coordinates.push([i, j]);
+    }
+  }
+  // array of tiles objects to save grid position and assigned number
+  let tilesObjectArray = [];
+  for (let i = 0; i < gridTiles.length; i++){
+    let tilesObject = {};
+    tilesObject.position = coordinates[i];
+    tilesObject.number = gridTiles[i].textContent;
+    tilesObjectArray.push(tilesObject);
+  }
+  console.log(tilesObjectArray);
+  return tilesObjectArray;
+}
 
+getTilesObjectArray(gridTiles);
 
 function runGame() {
 
