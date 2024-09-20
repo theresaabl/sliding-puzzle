f1df39828e85b612a550184cad6a82d34db84412
Theresa Abl <theresaabl@gmx.at>
2024-09-20 09:52:37 +0000

    Add click event listeners to tiles with test function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,47 @@
 /* jshint esversion: 11 */
 
+
+// Add event listeners for tiles
+
+// HTML collection of tiles
+let gridPositions = document.getElementsByClassName("position");
+console.log(gridPositions);
+// 1d array of tiles
+let gridSize = 3;
+let tilesArray1d = [];
+for (let tile of gridPositions){
+  tilesArray1d.push(tile.textContent);
+}
+console.log(tilesArray1d);
+// grid coordinates for gridSize
+let coordinates = [];
+for (let i = 0; i < gridSize; i ++){
+  coordinates.push([]);
+
+}
+
+// // object of tiles
+// let tilesObject = {};
+// for (let tile of gridPositions){
+//   tilesObject["position"] = [ , ];
+//   tilesObject["number"] = tile.textContent;
+// }
+// console.log(tilesObject);
+
+for (let tile of gridPositions) {
+  tile.addEventListener("click", function() {
+    this.style.backgroundColor="red";
+    console.log(this);
+    // let isNeighbour = isNeighbourEmpty(this);
+    // let emptyTile = document.getElementsByClassName("empty-tile");
+    // if (isNeighbourEmpty(this)){
+    //   moveTile(this);
+    // }
+  });
+}
+
+
+
 function runGame() {
 
 }
