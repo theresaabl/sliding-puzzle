1c4a3ff44ba64da36290bd21e7663cd9de3dbb46
Theresa Abl <theresaabl@gmx.at>
2024-09-20 08:39:59 +0000

    Set up structure of game functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,42 @@
 /* jshint esversion: 11 */
+
+function runGame() {
+
+}
+
+function getGridSize() {
+
+}
+
+function shuffle() {
+
+}
+
+function isSolvable() {
+
+}
+
+function getTile() {
+
+}
+
+function isNeighbourEmpty() {
+
+}
+
+function moveTile() {
+
+}
+
+function incrementMoveCounter() {
+
+}
+
+function isPuzzleSolved() {
+
+}
+
+function winMessage() {
+
+}
+
