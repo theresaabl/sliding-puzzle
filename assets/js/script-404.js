70127e8307aeb02f459099db40e9e6885e0b97a2
Theresa Abl <theresaabl@gmx.at>
2024-09-29 05:14:23 +0000

    Create error message with function

--- assets/js/script-404.js
+++ assets/js/script-404.js
@@ -10,11 +10,12 @@ document.addEventListener("DOMContentLoaded", function() {
 function errorMessage(){
     let error = document.getElementById("error-message");
     let errorHTML = "";
+    let errorString = `Error404Oops this page does not exist To return to game click `;
     let gridSize = 8;
-    for (let i = 1; i < gridSize * gridSize; i++) {
-      errorHTML += `<div class="tile-style tile-js"><p>${i}</p></div>`;
+    for (let i = 0; i < errorString.length; i++) {
+      errorHTML += `<div class="tile-style"><p>${errorString[i]}</p></div>`;
     }
-    errorHTML += '<div class="empty-tile tile-js"><p>0</p></div>';
+    errorHTML += '<div class="tile-style"><p><a href="index.html"><i class="fa-solid fa-puzzle-piece" title="Go to game"></i></a></p></div>';
     document.getElementById("error-message").innerHTML = errorHTML;
     //set styles for grid
     //calculate column-width depending on gridSize as a percentage
